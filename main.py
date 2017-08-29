314b3ab191408f58daaec9c04f830bd6214b6557
Derek Wisong <derekwisong@gmail.com>
2017-08-29 12:39:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import geo
+import logging
 
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    log.info("Running the ADSB check script")
