e5c44b53d518aea1b1d436c8a0a63b515aedf16b
Derek Wisong <derek@wisong.net>
2017-08-30 13:09:44 -0400

    updated code to remove some adsbexchange reusable functions into a module

---
+++ adsbexchange.py
@@ -0,0 +1,20 @@
+import zipfile
+import logging
+import json
+
+log = logging.getLogger(__name__)
+
+def extract_data(zipped_data, filename):
+    f = zipped_data.read(filename).decode('utf-8')
+    return f
+
+def parse_data(zip_filepath, filename):
+    log.info("Looking for aircraft in {}".format(filename))
+
+    with zipfile.ZipFile(zip_filepath) as zipped_data:
+        data = json.loads(extract_data(zipped_data, filename))
+        return data
+
+def get_file_list(zip_filepath):
+    with zipfile.ZipFile(zip_filepath) as z:
+        return z.namelist()
